00490e11d5fc3603b9db89c0735303fe92ed5e5b
hannah smith <hannahhsmith12@email.arizona.edu>
2019-04-24 14:42:56 -0700

    lots of stuff

--- final_service.js
+++ final_service.js
@@ -4,7 +4,7 @@
 // 		cleanliness (out of 10)
 //		hours
 //		stalls vs single
-//		"by costumers only"
+//		"by customers only"
 //		baby accomadations
 //		unisex option
 //		wheelchair accessibility
@@ -42,15 +42,53 @@ function readFile(fileName) {
 	}
 	return file;
 }
+
+/**
+	getBasic(directory) : This function reads the info.txt file in the specified 
+	folder and returns the first 3 lines as a JSON object.
+*/
+function getBasic(directory) {
+	let basic = readFile("places/" + directory + "/info.txt");
+	let lines = basic.split('\n');
+	let place = {"folder" : directory, "name" : lines[0], "address" : lines[1], "type" : lines[2],
+		"rating" : lines[3]}
+	return place;
+}
+
 /**
 	getInfo(directory) : This function reads the info.txt file in the specified 
-	folder and returns the info as a JSON object.
+	folder and returns the contents as a JSON object.
 */
 function getInfo(directory) {
 	let info = readFile("places/"+directory+"/info.txt");
 	let lines = info.split('\n');
-	let place = {"name": lines[0], "address": lines[1], "rating": lines[2]}
+	let place = {"folder" : directory, "name" : lines[0], "address" : lines[1], "type": lines[2],
+		"rating" : lines[3], "hours" : lines[4], "style" : lines[5], "CO" : lines[6], 
+		"baby" : lines[7], "unisex" : lines[8], "wheelchair" : lines[9], "family" : lines[10]};
+	// style = stalls or single
+	// CO = customers only
 	return place;
+}
+
+function getReviews(directory) {
+	let file = readFile("places/" + directory + "/reviews.txt");
+	let lines = file.split('\n');
+	let reviews = [];
+	for (let i = 0; i < lines.length; i ++) {
+		let info = lines[i].split(':::');
+		reviews[i] = {"folder" : directory, "name": info[0], "rating": info[1], "review": info[2]};
+	}
+	return {reviews};
+}
+
+function getAllBasic() {
+	let folders = fs.readdirSync("places");
+	let places = [];
+	for (let i = 1; i < folders.length; i ++) {
+		let info = getBasic(folders[i]);
+		places[i-1] = info;
+	}
+	return places;
 }
 
 // GET:
@@ -62,19 +100,83 @@ app.get('/', function (req, res) {
 	let mode = req.query.mode;
 	let location = req.query.place;
 
-	let places = fs.readdirSync("places");
+	//let folders = fs.readdirSync("places");
+	//console.log(files);
 	if (mode === "listinfo") {
+		console.log("listinfo");
 		let places = [];
 		let directories = fs.readdirSync("places");
 		for (let i = 1; i < directories.length; i ++) {
-			let folder = directories[i];
-			let place = getInfo(folder);
+			let place = getBasic(directories[i]);
 			places[i-1] = place;
-			console.log("place: " + places[i]);
 		}
-		places= {places};
+		places = {places};
+		console.log(places);
 		res.send(JSON.stringify(places));
+	} else if (mode === "moreinfo") {
+		let info = getInfo(location);
+		res.send(JSON.stringify(info));
+	} else if (mode === "reviews") {
+		let reviews = getReviews(location);
+		console.log(reviews);
+		res.send(JSON.stringify(reviews));
+	} else if (mode === "map") {
+		let places = getAllBasic();
+		console.log("addresses: "+places);
+		res.send(JSON.stringify({places}));
 	}
+});
+
+// POST:
+app.post('/', jsonParser, function (req, res) {
+	res.header("Access-Control-Allow-Origin", "*");
+	let folder = req.body.folder;
+	let name = req.body.name;
+	let address = req.body.address;
+	let type = req.body.type;
+	let rating = req.body.rating;
+	let hours = req.body.hours;
+	let style = req.body.style;
+	let customers = req.body.customers;
+	let baby = req.body.baby;
+	let unisex = req.body.unisex;
+	let wheelchair = req.body.wheelchair;
+	let family = req.body.family;
+	let comment = req.body.comment;
+	let first = req.body.first;
+	let last = req.body.last;
+
+	let info = name + "\n" + address + "\n" + type + "\n" + rating + "\n" +
+		hours + "\n" + style + "\n" + customers + "\n" + baby + "\n" + unisex
+		+ "\n" + wheelchair + "\n" + family;
+
+	let dir = process.cwd() + "/places/" + folder;
+	if (!fs.existsSync(dir)) {
+		fs.mkdirSync(dir);
+	}
+
+	// write info file:
+	fs.writeFile(process.cwd() + "/places/" + folder + "/info.txt", info, function(err) {
+		if(err) {
+			console.log(err);
+			res.status(400);
+			res.send('Error submitting review.');
+		}
+	});
+
+	// write review file:
+	let review = first + last[0] + ".:::" + rating + ":::" + comment;
+	fs.appendFile(process.cwd() + "/places/" + folder + "/reviews.txt", review, function(err) {
+		if(err) {
+			console.log(err);
+			res.status(400);
+			console.log("error");
+			res.send('Error submitting review.');
+		}
+		console.log("success");
+		res.send('Review successfully submitted!');
+	});
+
 });
 
 app.listen(3000);
